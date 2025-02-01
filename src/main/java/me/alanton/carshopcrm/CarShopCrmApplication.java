32ea6788e525f2f524655a7e1234c7782c28c05c
alankasymbek <alankassymbek@gmail.com>
2025-02-02 01:35:23 +0500

    Initial commit

---
+++ src/main/java/me/alanton/carshopcrm/CarShopCrmApplication.java
@@ -0,0 +1,13 @@
+package me.alanton.carshopcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShopCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarShopCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/alanton/carshopcrm/CarShopCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alanton.carshopcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShopCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
