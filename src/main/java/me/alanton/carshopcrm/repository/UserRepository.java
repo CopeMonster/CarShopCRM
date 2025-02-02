9f21842c005208b0cd9980f9d266a6951d7ff5f3
alankasymbek <alankassymbek@gmail.com>
2025-02-02 10:24:36 +0500

    UserRepository was added

---
+++ src/main/java/me/alanton/carshopcrm/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package me.alanton.carshopcrm.repository;
+
+import me.alanton.carshopcrm.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
