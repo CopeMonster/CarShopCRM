f7153e97bfe66e4997679751a03b34f0317873f8
alankasymbek <alankassymbek@gmail.com>
2025-02-02 10:17:18 +0500

    User was added

---
+++ src/main/java/me/alanton/carshopcrm/entity/User.java
@@ -0,0 +1,33 @@
+package me.alanton.carshopcrm.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+
+@Entity
+@Table(name = "\"user\"")
+public class User extends BaseEntity {
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstname;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastname;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
+
