96601d95b692856bac27995968dd5683c44e4022
alankasymbek <alankassymbek@gmail.com>
2025-02-02 20:29:11 +0500

    RoleInitializer was added

---
+++ src/main/java/me/alanton/carshopcrm/bootstrap/RoleInitializer.java
@@ -0,0 +1,34 @@
+package me.alanton.carshopcrm.bootstrap;
+
+import lombok.RequiredArgsConstructor;
+import me.alanton.carshopcrm.dto.request.RoleRequest;
+import me.alanton.carshopcrm.service.RoleService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class RoleInitializer implements CommandLineRunner {
+    private final RoleService roleService;
+    private final String[] DEFAULT_ROLES = {
+            "SYS_ADMIN",
+            "ADMIN",
+            "OPERATOR",
+            "MANAGER",
+            "HEAD_OF_SALES",
+            "MARKETER",
+            "HEAD_OF_SERVICE",
+            "SERVICE_MANAGER"
+    };
+
+    @Override
+    public void run(String... args) throws Exception {
+        for (String role : DEFAULT_ROLES) {
+            if (!roleService.isRoleExistByName(role)) {
+                RoleRequest roleRequest = new RoleRequest(role);
+
+                roleService.createRole(roleRequest);
+            }
+        }
+    }
+}
