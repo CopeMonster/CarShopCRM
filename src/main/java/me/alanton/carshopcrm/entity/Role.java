18b17bcf98ec1a9fbb533e8fa140a4be34f15f51
alankasymbek <alankassymbek@gmail.com>
2025-02-02 12:46:42 +0500

    Default values for Set and List were added

--- src/main/java/me/alanton/carshopcrm/entity/Role.java
+++ src/main/java/me/alanton/carshopcrm/entity/Role.java
@@ -4,12 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -21,9 +19,15 @@ import java.util.List;
 @Entity
 @Table(name = "role")
 public class Role extends BaseEntity {
-    @Column(name = "role", unique = true, nullable = false)
+    @Column(name = "name", unique = true, nullable = false)
     private String name;
 
     @ManyToMany(mappedBy = "roles")
-    private List<User> users;
+    @Builder.Default
+    private List<User> users = new ArrayList<>();
+
+    public Role(String name) {
+        this.name = name;
+        users = new ArrayList<>();
+    }
 }
--- src/main/java/me/alanton/carshopcrm/entity/User.java
+++ src/main/java/me/alanton/carshopcrm/entity/User.java
@@ -1,12 +1,10 @@
 package me.alanton.carshopcrm.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
@@ -36,6 +34,19 @@ public class User extends BaseEntity {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
-    private Set<Role> roles;
+    @Builder.Default
+    private Set<Role> roles = new HashSet<>();
+
+    public User(
+            String email,
+            String firstname,
+            String lastname,
+            String password) {
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+        this.roles = new HashSet<>();
+    }
 }
 
