74ccbf05c499dfdc876f821edbcc13d66c3cd40b
alankasymbek <alankassymbek@gmail.com>
2025-02-02 23:36:12 +0500

    ID was removed from UserRequest

--- src/main/java/me/alanton/carshopcrm/dto/request/UserRequest.java
+++ src/main/java/me/alanton/carshopcrm/dto/request/UserRequest.java
@@ -3,11 +3,9 @@ package me.alanton.carshopcrm.dto.request;
 import lombok.Builder;
 
 import java.util.List;
-import java.util.UUID;
 
 @Builder
 public record UserRequest(
-        UUID id,
         String email,
         String firstname,
         String lastname,
