33f78bac569a5ab1d8441fbbc689e30494da824f
alankasymbek <alankassymbek@gmail.com>
2025-02-02 21:17:21 +0500

    RefreshTokenRepository was added

---
+++ src/main/java/me/alanton/carshopcrm/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package me.alanton.carshopcrm.repository;
+
+import me.alanton.carshopcrm.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    Optional<RefreshToken> findByIdAndTokenExpiredAt(UUID id, Instant tokenExpiredAt);
+}
